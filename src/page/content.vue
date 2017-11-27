238aad654aa30a03d373b6e13c89f8fafaf5f51c
RUYU\rysh102 <xiongjuhui@linglie.com>
2017-11-27 18:08:48 +0800

    提交代码

---
+++ src/page/content.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>我是content页面123</div>
+</template>
+<script>
+	import Hello from '../components/HelloWorld.vue'
+
+	export default {
+		components: {Hello},
+		data() {
+			return {}
+		},
+		created() {},
+		methods: {
+			
+		},
+	}
+
+</script>
+<style>
+	
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h4>
+			{{msg}}
+		</h4>
+	</div>
+</template>
+<script>
+	export default {
+	  data () {
+	    return {
+	      msg: '我是index页面，不是写死的哦'
+	    }
+	  },
+	  created () {
+	    this.$api.get('topics', null, r => {
+	      console.log(r)
+	    })
+	  }
+	}
+</script>
+<style>
+	h4 {
+		height: 30px;
+		line-height: 30px;
+		font-size: 16px;
+	}
+</style>
